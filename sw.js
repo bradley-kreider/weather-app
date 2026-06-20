5566781968342aa90c544a6ad119d74716c4c07b
bradley-kreider <bradk1144@gmail.com>
2026-06-20 17:45:18 -0400

    Fix gradient on mobile: move orbs to html element with background-attachment fixed

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'weather-v3';
+const CACHE = 'weather-v4';
 const SHELL = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
